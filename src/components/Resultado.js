428ffed8a17eec807de9c64c5d33365caf860fb4
laskydev <eduardofortuna99@gmail.com>
2021-03-23 21:06:28 -0600

    finalizado

---
+++ src/components/Resultado.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from '@emotion/styled'
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
+import { ENTERED } from 'react-transition-group/Transition';
+const Mensaje = styled.p`
+    background-color: rgb(127,224,237);
+    margin-top: 2rem;
+    padding: 1rem;
+    text-align: center;
+`
+const ResultadoCotizacion = styled.div`
+    text-align: center;
+    padding: .5rem;
+    border: 1px solid #26c6da;
+    background-color: rgb(127,224,237);
+    margin-top: 1rem;
+    position: relative;
+`
+const TextoCotizacion = styled.p`
+    color: #00838F;
+    padding: 1rem;
+    text-transform: uppercase;
+    font-weight: bold;
+    margin:0;
+`
+const Resultado = ({cotizacion}) => {
+    return(
+        (cotizacion === 0) 
+        ? <Mensaje>Elige marca, año y tipo de seguro</Mensaje> 
+        : <ResultadoCotizacion>
+            <TransitionGroup
+                component="p"
+                className="resultado"
+            >
+                <CSSTransition
+                    classNames="resultado"
+                    key={cotizacion}
+                    timeout={{enter:1000, exit:1000}}
+                >
+                    <TextoCotizacion>El total es: ${cotizacion}</TextoCotizacion>
+                </CSSTransition>
+            </TransitionGroup>
+        </ResultadoCotizacion>
+    )
+}
+
+export default Resultado;
---
+++ src/components/Spinner.js
@@ -0,0 +1,15 @@
+import './Spinner.css'
+const Spinner = () => {
+    return (
+        <div className="sk-chase">
+            <div className="sk-chase-dot" />
+            <div className="sk-chase-dot" />
+            <div className="sk-chase-dot" />
+            <div className="sk-chase-dot" />
+            <div className="sk-chase-dot" />
+            <div className="sk-chase-dot" />
+        </div>
+    );
+};
+
+export default Spinner;
